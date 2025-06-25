02d94954947b4d77fdf6ca60f4e2d8874f2a0a68
Donghyun Lee <leedonghyun022@gmail.com>
2025-06-25 15:42:49 +0900

    feat: 행성 렌더링 추가

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,37 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getRandomColor = (minBrightness: number): string => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+  if (brightness < minBrightness) {
+    return getRandomColor(minBrightness);
+  }
+
+  return `rgb(${r}, ${g}, ${b})`;
+};
+
+export const getRandomVector = (size: number): [number, number, number] => {
+  let x = Math.random() - 0.5;
+  let y = Math.random() - 0.5;
+  let z = Math.random() - 0.5;
+
+  const length = Math.sqrt(x * x + y * y + z * z);
+  if (length === 0) {
+    return getRandomVector(size);
+  }
+
+  // Normalize the vector
+  x = (x / length) * size;
+  y = (y / length) * size;
+  z = (z / length) * size;
+
+  return [x, y, z];
+};
--- src/pages/space.tsx
+++ src/pages/space.tsx
@@ -1,7 +1,85 @@
 /* eslint-disable react/no-unknown-property */
-import { OrbitControls, Sphere, Stars } from "@react-three/drei";
+import {
+  Billboard,
+  OrbitControls,
+  Sphere,
+  Stars,
+  Text,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { Bloom, EffectComposer } from "@react-three/postprocessing";
+import {
+  EffectComposer,
+  Select,
+  Selection,
+  SelectiveBloom,
+} from "@react-three/postprocessing";
+
+import { getRandomColor, getRandomVector } from "~/lib/utils";
+
+const data: {
+  color: string;
+  id: number;
+  name: string;
+  userScore: number;
+  vector: [number, number, number];
+}[] = [
+  {
+    color: getRandomColor(100),
+    id: 1,
+    name: "삼성전자",
+    userScore: 85,
+    vector: getRandomVector(1),
+  },
+  {
+    color: getRandomColor(100),
+    id: 2,
+    name: "LG전자",
+    userScore: 90,
+    vector: getRandomVector(1),
+  },
+  {
+    color: getRandomColor(100),
+    id: 3,
+    name: "네이버",
+    userScore: 78,
+    vector: getRandomVector(1),
+  },
+  {
+    color: getRandomColor(100),
+    id: 4,
+    name: "카카오",
+    userScore: 88,
+    vector: getRandomVector(1),
+  },
+  {
+    color: getRandomColor(100),
+    id: 5,
+    name: "현대자동차",
+    userScore: 92,
+    vector: getRandomVector(1),
+  },
+  {
+    color: getRandomColor(100),
+    id: 6,
+    name: "기아",
+    userScore: 80,
+    vector: getRandomVector(1),
+  },
+  {
+    color: getRandomColor(100),
+    id: 7,
+    name: "SK텔레콤",
+    userScore: 75,
+    vector: getRandomVector(1),
+  },
+  {
+    color: getRandomColor(100),
+    id: 8,
+    name: "LG디스플레이",
+    userScore: 82,
+    vector: getRandomVector(1),
+  },
+];
 
 export const Space = () => {
   return (
@@ -13,10 +91,19 @@ export const Space = () => {
         {/* Ambient + Point Light */}
         <ambientLight intensity={0.05} />
         <pointLight color="white" intensity={3} position={[0, 0, 0]} />
-
         {/* 태양 */}
-        <Planet color="#fff437" position={[0, 0, 0]} radius={0.1} />
-
+        <Selection>
+          <EffectComposer autoClear={false}>
+            <SelectiveBloom
+              intensity={2}
+              luminanceSmoothing={0.9}
+              luminanceThreshold={0.1}
+            />
+          </EffectComposer>
+          <Select enabled>
+            <Planet color="#fff437" position={[0, 0, 0]} radius={0.1} />
+          </Select>
+        </Selection>
         {/* 별 배경 */}
         <Stars
           count={3000}
@@ -26,17 +113,40 @@ export const Space = () => {
           radius={100}
           saturation={0}
         />
-
-        <OrbitControls enablePan enableRotate enableZoom />
-
-        {/* 빛 퍼짐 효과 */}
-        <EffectComposer>
-          <Bloom
-            intensity={2}
-            luminanceSmoothing={0.9}
-            luminanceThreshold={0.1}
-          />
-        </EffectComposer>
+        {data.map((item) => (
+          <>
+            <Planet
+              color={item.color}
+              key={item.id}
+              position={
+                item.vector.map((v) => ((v * item.userScore) / 100) * 4) as [
+                  number,
+                  number,
+                  number,
+                ]
+              }
+              radius={0.05}
+            />
+            <PlanetMarker
+              key={`marker-${item.id}`}
+              name={item.name}
+              position={
+                item.vector.map((v) => ((v * item.userScore) / 100) * 4) as [
+                  number,
+                  number,
+                  number,
+                ]
+              }
+            />
+          </>
+        ))}
+        <OrbitControls
+          autoRotate
+          autoRotateSpeed={0.5}
+          enablePan
+          enableRotate
+          enableZoom
+        />
       </Canvas>
     </div>
   );
@@ -55,5 +165,30 @@ const Planet = ({
     <Sphere args={[radius, 64, 64]} position={position}>
       <meshStandardMaterial emissive={color} />
     </Sphere>
+  );
+};
+
+const PlanetMarker = ({
+  name = "VENUS",
+  position = [0, 0, 0],
+}: {
+  name: string;
+  position: [number, number, number];
+}) => {
+  return (
+    <Billboard position={position}>
+      {/* 텍스트 표시 */}
+      <Text
+        anchorX="center"
+        anchorY="middle"
+        color="white"
+        fontSize={0.2}
+        outlineColor="#000"
+        outlineWidth={0.005}
+        position={[0, -0.3, 0]} // 아래에 표시
+      >
+        {name}
+      </Text>
+    </Billboard>
   );
 };
