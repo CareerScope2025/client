779af1954dc15e95973118d7f1165fd8cb6c3709
Donghyun Lee <leedonghyun022@gmail.com>
2025-06-25 13:03:21 +0900

    feat: eslint, pretteri

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,7 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+// @ts-check
+import { defineConfig } from "eslint-config-react-app-essentials";
 
-export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-  },
-])
+export default defineConfig({
+  tsconfigRootDir: "./tsconfig.json",
+  scope: ["src/**/*.{ts,tsx}"],
+});
